cf118a5242418736eaf06f95253d1836eb11524b
Abdulsalam24 <abdulsalammohammed586686@gmail.com>
2023-03-13 23:21:17 +0100

    private endpoint added

--- controller/authRouter.js
+++ controller/authRouter.js
@@ -6,9 +6,11 @@ import dotenv from 'dotenv/config'
 
 const router = express.Router()
 
+
+
 router.post("/signup", async (req, res) => {
     const { email, password, role } = req.body
-    
+
     if (!(email && password && role)) {
         res.status(401).json({ message: "All filed are required" })
         return;
@@ -44,34 +46,47 @@ router.post("/signup", async (req, res) => {
         role,
     })
 
-    if(newUser){
+    if (newUser) {
         res.status(201).json({
-            email : newUser.email,
-            password : newUser.password,
-            role : newUser.role,
-            token : generateToken(newUser.id)
+            email: newUser.email,
+            password: newUser.password,
+            role: newUser.role,
+            token: generateToken(newUser.id)
         })
     }
-    else{
-        res.status(400).json({message : "Wrong user data"})
+    else {
+        res.status(400).json({ message: "Wrong user data" })
     }
 })
-
 
 router.post("/signin", async (req, res) => {
     const { email, password } = req.body
     const user = await User.findOne({ email })
 
     if (user && await bcrypt.compare(password, user.password)) {
-        res.status(201).json({ token : generateToken(user.id), message: "signin successfully" })
+        res.status(201).json({ token: generateToken(user.id), message: "signin successfully" })
     } else {
         res.status(401).json({ message: "wrong user credentials" })
     }
 })
 
+router.get("/getme", async (req, res) => {
+    let token
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try {
+            token = req.headers.authorization.split(' ')[1]
+            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+            req.user = await User.findById(decoded.id)
+        } catch (error) {
+            console.log(error, 'errorerror')
+            res.status(404).json({ message: "unAuthorized" })
+        }
+    }
+    res.status(200).json({ id: req.user._id, email: req.user.email, role: req.user.role })
+})
 
 const generateToken = (id) => {
-    return jwt.sign({id} , process.env.JWT_SECRET , {expiresIn : "5h"})
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "5h" })
 }
 
 export default router
